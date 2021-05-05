9bb568a4c30ebb62716f66e9938d40e82f70c1b3
Robert Israelsson <israelsson.r@gmail.com>
2021-05-05 22:13:13 +0200

    Added sql file with commands to populate book data into database

---
+++ src/add_books.sql
@@ -0,0 +1,8 @@
+
+DELETE FROM book;
+
+INSERT INTO book VALUES(null, "What Happened to You?", 9781529068467, "Oprah Winfrey", "img/what-happened-to-you.jpeg");
+
+INSERT INTO book VALUES(null, "Tills alla dör", 9789189061842, "Diamant Salihu", "img/tills-alla-dor.jpeg");
+
+INSERT INTO book VALUES(null, "En ensam plats", 9789113115719, "Kristina Sandberg", "img/en-ensam-plats.jpeg");
